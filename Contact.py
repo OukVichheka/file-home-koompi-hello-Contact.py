0b702ee7a784b97fe97480dd4a57e7218ee8682b
OukVichheka <42796794+OukVichheka@users.noreply.github.com>
2019-02-28 14:29:19 +0700

    Add files via upload

---
+++ Contact.py
@@ -0,0 +1,12 @@
+def edit_contact():
+    print('1.Edit Name')
+    print('2.Edit PhoneNumber')
+    print('0.Exit')
+    emenu=input('>>>')
+    if emenu=='1':
+        NewName=
+    elif ememnu=='2':
+        
+    elif emenu=='0':
+        break
+    return
